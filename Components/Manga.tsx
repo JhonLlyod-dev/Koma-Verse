5e9225de25b5bbabdc22ab30a10a511982e6ddb5
jhon-dev <navarro.jhonllyod2005@gmail.com>
2026-01-26 08:03:27 +0800

    finished the item

--- Components/Manga.tsx
+++ Components/Manga.tsx
@@ -1,15 +1,14 @@
 
-import { getMangaList } from "@/Service/mangadex-api";
+
 import Card from "./Card";
 import { getMangaTitle, getMangaDesc } from "@/utils/utils";
 import { getCoverUrl } from "@/utils/utils";
+type MangaProps = {
+  mangaList: any[];
+};
+export default async function Manga({ mangaList }: MangaProps) {
+ 
 
-export default async function Manga() {
-  const data = await getMangaList();
-  
-  // const Top1 = data.data[0]; i want this on parent component
-  // fetch ONCE with cover_art included
-  const mangaList = data.data.slice(1);
   return (
     <div className="flex-1 sm:flex-1/3 lg:flex-1/2 w-full flex-center flex-wrap gap-8 md:gap-4 md:gap-y-12">
       {mangaList.map((manga: any) => {
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,25 +2,27 @@
 import Manga from "@/Components/Manga";
 import Top10 from "@/Components/Top10";
 import { FireIcon } from "@heroicons/react/16/solid";
+import { getMangaList } from "@/Service/mangadex-api";
 
-export default function Home() {
 
+export default async function Home() {
 
-  function top1(){
+ const data = await getMangaList();
+ const top1 = data.data[0];
 
-  // 11111
-  }
+ const rest = data.data.slice(1);
+
 
   return (
     <main className=" min-h-screen flex flex-col bg-background pb-10">
         
 
       <div className="bg-gray-400">
-        Top 1 Latest
+        Top 1 Latest {top1.id}
       </div>
 
       <div className="flex justify-center flex-col md:flex-row  my-5 p-2 md:p-4 padding gap-y-4">
-        <Manga />
+        <Manga mangaList={rest} />
         <div className="ml-2 flex-1 px-4 md:px-0 flex flex-col gap-2  md:max-w-100 ">
           <div className="flex-center justify-start gap-2">
             <h2 className="text-2xl font-bold">Hottest</h2>
